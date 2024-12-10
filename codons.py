ad72bd379deed72b804839ab8ac3a3f8776530e3
yuvaltal800 <yuvaltal800@gmail.com>
2024-12-10 22:41:44 +0200

    Update codons.py

--- codons.py
+++ codons.py
@@ -1,6 +1,3 @@
-!mkdir data # Create a folder for the data in the colab
-!wget https://raw.githubusercontent.com/yotam-biu/ps6/main/data/codons.txt -O /content/data/codons.txt
-
 def creat_codon_dict(file_path):
   file = open(file_path)
   rows = file.readlines()
