b4e01a04999032a00ce6ffb4e68eaf50cb879493
yuvaltal800 <yuvaltal800@gmail.com>
2024-12-10 22:41:24 +0200

    Update codons.py

--- codons.py
+++ codons.py
@@ -1,9 +1,8 @@
 !mkdir data # Create a folder for the data in the colab
 !wget https://raw.githubusercontent.com/yotam-biu/ps6/main/data/codons.txt -O /content/data/codons.txt
 
-def create_codon_dict(file_path):
-  path="data/codons.txt"
-  file = open(path)
+def creat_codon_dict(file_path):
+  file = open(file_path)
   rows = file.readlines()
   file.close()
 
@@ -13,5 +12,4 @@ def create_codon_dict(file_path):
     codon=clean[0]
     amino_acid_abbreviation=clean[2]
     codon_to_amino[codon] = amino_acid_abbreviation
-
-   codon_to_amino= {file(path)}
+ return codon_to_amino
