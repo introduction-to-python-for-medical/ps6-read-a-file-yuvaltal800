ff5bb710e503c6daf65bbe4ac3caaba5d9e86f5b
yuvaltal800 <yuvaltal800@gmail.com>
2024-12-11 17:30:57 +0200

    Update codons.py

--- codons.py
+++ codons.py
@@ -2,11 +2,10 @@ def creat_codon_dict(file_path):
   file = open(file_path)
   rows = file.readlines()
   file.close()
-
   codon_to_amino= {}
   for row in rows:
     clean=row.strip().split('\t')
     codon=clean[0]
     amino_acid_abbreviation=clean[2]
     codon_to_amino[codon] = amino_acid_abbreviation
-  return codon_to_amino
+return codon_to_amino
